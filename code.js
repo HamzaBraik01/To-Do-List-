a343f8294cc61cd5d1287247ca97ec7b77ee7282
HamzaBraik01 <hamzabraik02@gmail.com>
2024-11-01 21:36:32 +0100

    style de priorite de la tache

--- code.js
+++ code.js
@@ -22,20 +22,7 @@ function saveTask(event) {
     const date = document.getElementById('taskDate').value;
     const priority = document.getElementById('taskPriority').value;
 
-    const taskHtml = `
-        <div class="border border-gray-300 shadow-lg rounded-lg bg-white p-4 mb-4 transition-transform transform hover:scale-105">
-            <h3 class="text-xl font-semibold text-teal-500 mb-2">Task: <span class="text-gray-800">${title}</span></h3>
-            <p class="text-gray-700 mb-2">
-                <strong>Description:</strong> ${description}
-            </p>
-            <p class="text-gray-600 mb-1">
-                <strong>Date:</strong> ${date}
-            </p>
-            <p class="text-gray-600">
-                <strong>Priorité:</strong> <span class="font-bold text-orange-500">${priority}</span>
-            </p>
-        </div>
-    `;
+    
 
     let columnId;
     let countId;
@@ -49,14 +36,52 @@ function saveTask(event) {
         columnId = "doneColumn";
         countId = "doneCount";
     }
+        
+    const taskItem = document.createElement('div');
+    taskItem.className = "border border-gray-300 shadow-lg rounded-lg bg-white p-4 mb-4 transition-transform transform hover:scale-105 max-w-full break-words";
 
     
+    switch (priority) {
+        case 'P1':
+            taskItem.classList.add('border-l-8', 'border-red-500');
+            break;
+        case 'P2':
+            taskItem.classList.add('border-l-8', 'border-orange-500');
+            break;
+        case 'P3':
+            taskItem.classList.add('border-l-8', 'border-green-500');
+            break;
+    }
+
+    
+    taskItem.innerHTML = `
+        <h3 class="text-xl font-semibold text-teal-500 mb-2">Task: <span class="text-gray-800">${title}</span></h3>
+        <!--<p class="text-gray-700 mb-2 break-words">
+            <strong>Description:</strong> ${description}
+        </p>-->
+        <p class="text-gray-600 mb-1">
+            <strong>Date:</strong> ${date}
+        </p>
+        <!--<p class="text-gray-600">
+            <strong>Priorité:</strong> <span class="font-bold">${priority}</span>
+        </p>-->
+        <!--<div class="flex space-x-4">
+                <button onclick="editTask()" class="text-sm text-blue-500">Edit</button>
+                <button onclick="deleteTask()" class="text-sm text-red-500">Delete</button>
+        </div>-->
+    `;
+
     const column = document.getElementById(columnId);
-    column.innerHTML += taskHtml; 
+    column.appendChild(taskItem);
 
-    
+
+    const countElement = document.getElementById(countId);
+    let currentCount = parseInt(countElement.textContent.split(": ")[1]); 
+    currentCount += 1; 
+    countElement.textContent = `${countElement.textContent.split(": ")[0]}: ${currentCount}`; 
     
 
     
     toggleModal();
 }
+
